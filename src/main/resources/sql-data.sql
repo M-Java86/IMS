99665ca735bd51a830d0ad2e101653ee553badbe
M-java86 <mpythondata@outlook.com>
2021-06-01 12:35:19 -0400

    MO: fixed error in VALUES

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES (`raquel`, `hardy');
+INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('raquel', 'hardy');
+
+INSERT INTO 
