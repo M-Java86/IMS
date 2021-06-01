ea23d663c92845ea073a8c4ef8f56ea010a9c887
M-java86 <mpythondata@outlook.com>
2021-06-01 12:43:20 -0400

    MO: inserted ims.items and values

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,4 +1,6 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('raquel', 'hardy');
 
-INSERT INTO 
+INSERT INTO `ims.items` (`items_name`, `cost`) VALUES ('shoe', 80.00);
+INSERT INTO `ims.items` (`items_name`, `cost`) VALUES ('magazine`, 12.99);
+
