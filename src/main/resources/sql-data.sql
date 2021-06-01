f07bb10f56161337913a3d4d7cf2afcae93b5a4a
M-java86 <mpythondata@outlook.com>
2021-06-01 14:50:04 -0400

    MO: added inderted into order_customer

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,6 +1,9 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('raquel', 'hardy');
 
-INSERT INTO `ims.items` (`items_name`, `cost`) VALUES ('shoe', 80.00);
-INSERT INTO `ims.items` (`items_name`, `cost`) VALUES ('magazine`, 12.99);
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('shoe', 89.99);
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('magazine', 12.99);
 
+INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
+INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
+ 
