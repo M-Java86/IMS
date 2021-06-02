fa4314bdd00629bfcbd798ca086503f1e9fb31fa
M-java86 <mpythondata@outlook.com>
2021-06-01 21:21:31 -0400

    MO: inserted item values for new customers

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -4,7 +4,7 @@ INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('rodeny', 'jenni
 
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('shoe', 89.99);
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('magazine', 12.99);
-
+INSERT INTO `ims`. `items`(`item_name`, `price`) VALUES ('toy truck', 1.99);
 
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
