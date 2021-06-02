79732d1ba797320b71fdd26d096f30243b98bdb4
M-java86 <mpythondata@outlook.com>
2021-06-01 22:22:44 -0400

    MO: added DROP TABLE to accounts

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -18,25 +18,37 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
     PRIMARY KEY (`id`)
 
 );
-
+DROP TABLE IF EXISTS `order_customer`;
 CREATE TABLE IF NOT EXISTS `ims`. `order_customer` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `customer`_id INT(11) NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
     
-    
+);
+Drop TABLE IF EXISTS `order_customers`;
+CREATE TABLE IF NOT EXISTS `ims`.`order_customer` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE IF NOT EXISTS `order_products` (
+    `id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    `quantity` INT(11) NOT NULL,
+    FOREIGN KEY (`id`) REFERENCES `order_customer`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
 
 );
-
+DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE IF NOT EXISTS `ims`. 'accounts' (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `user_name` VARCHAR(40) NULL DEFAULT NULL,
     `password` VARCHAR(40) NULL DEFAULT NULL,
     `is_loggin` Boolean NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
-);
-
 
 
 
