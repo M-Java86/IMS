be25c3414ad6a133b48b7574bc84767236a5030a
M-java86 <mpythondata@outlook.com>
2021-06-01 21:26:36 -0400

    MO: made corrections

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -7,7 +7,6 @@ INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('magazine', 12.99);
 INSERT INTO `ims`. `items`(`item_name`, `price`) VALUES ('toy truck', 1.99);
 
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
-INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
 
 INSERT INTO `ims`.`order_products` (`id`, `item_id`, `quantity`) VALUES (1,1,1);
 INSERT INTO `ims`.`order_products`(`id`, `item_id`, `quantity`) VALUES(1,2,4);
@@ -15,6 +14,3 @@ INSERT INTO `ims`.`order_products`(`id`, `item_id`, `quantity`) VALUES(1,2,4);
 INSERT INTO `ims`.`order_products`(`id`,`item_id`, `quantity`) Values (2,3,1);
 
 INSERT INTO `ims`. `accounts` (`user_name`, `password`, `is_loggin`) VALUES ('root', 'root', true);
-INSERT INTO `ims`. `accounts` (`user_name`, `password`, `is_loggin`) VALUES ('another root', 'another root', fasle);
-
- 
