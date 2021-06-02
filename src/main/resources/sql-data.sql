906af949b1c746f70fe82555483fc256d782d28c
M-java86 <mpythondata@outlook.com>
2021-06-01 20:13:02 -0400

    MO: inserted acconts, username and password into root

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -6,4 +6,13 @@ INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('magazine', 12.99);
 
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
+
+INSERT INTO `ims`.`order_products` (`id`, `item_id`, `quantity`) VALUES (1,1,1);
+INSERT INTO `ims`.`order_products`(`id`, `item_id`, `quantity`) VALUES(1,2,4);
+
+INSERT INTO `ims`.`order_products`(`id`,`item_id`, `quantity`) Values (2,3,1);
+
+INSERT INTO `ims`. `accounts` (`user_name`, `password`, `is_loggin`) VALUES ('root', 'root', true);
+INSERT INTO `ims`. `accounts` (`user_name`, `password`, `is_loggin`) VALUES ('another root', 'another root', fasle);
+
  
