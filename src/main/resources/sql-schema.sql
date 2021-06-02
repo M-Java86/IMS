749d429b1eb278e7897be3c3b1b4e48308feb853
M-java86 <mpythondata@outlook.com>
2021-06-01 22:04:21 -0400

    MO: added DROP TABLE

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -3,14 +3,14 @@ drop schema ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
-
+DROP TABLE IF EXISTS 'customers';
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
-
+DROP TABLE IF EXISTS `items`
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `item_name` VARCHAR(40) Null Default Null,
