767a5865c26396d5c9c4bf8731edba97dfdf0a9b
M-java86 <mpythondata@outlook.com>
2021-06-01 21:18:45 -0400

    MO: inserted new customer into table

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,8 +1,10 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('raquel', 'hardy');
+INSERT INTO `ims`. `customers`(`first_name`, `surname`) VALUES ('rodeny', 'jennings');
 
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('shoe', 89.99);
 INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('magazine', 12.99);
+
 
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
 INSERT INTO `ims`.`order_customer`(`customer_id`) VALUES (1);
