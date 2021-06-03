a50fe253279154869e5794314aec779b220ce41b
M-java86 <mpythondata@outlook.com>
2021-06-03 13:40:37 -0400

    MO: AccountController

--- src/main/java/com/qa/ims/controller/AccountController.java
+++ src/main/java/com/qa/ims/controller/AccountController.java
@@ -1,5 +1,13 @@
 package com.qa.ims.controller;
 
-public class AccountController {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class AccountController<Account> implements CrudController<Account> {
+	
+	
+		public static final Logger Logger = LogManager.getLogger();
+	
 
 }
+
