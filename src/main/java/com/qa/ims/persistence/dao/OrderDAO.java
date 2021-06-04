c09d827c435b6752f6ae2ab674d6880b10d07f6e
M-java86 <mpythondata@outlook.com>
2021-06-03 20:41:26 -0400

    MO: adding Logger

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -0,0 +1,9 @@
+package com.qa.ims.persistence.dao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+public class OrderDAO {
+	public static final Logger LOGGER = LogManager.getLogger();
+
+}
