c67f70881351d49c5698f2bb264fd8d74819bedd
M-java86 <mpythondata@outlook.com>
2021-06-03 20:48:46 -0400

    MO: added previous code from items and customer for reference and setup

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -17,4 +17,118 @@ import com.qa.ims.utils.DBUtils;
 public class OrderDAO {
 	public static final Logger LOGGER = LogManager.getLogger();
 
+@Override
+public Items modelFromResultSet(ResultSet resultSet) throws SQLException {
+	Long id = resultSet.getLong("id");
+	String itemName = resultSet.getString("item_name");
+	double price = resultSet.getDouble("price");
+	return new Items(id, itemName, price);
+}
+
+/**
+ * Reads all items from the database
+ * 
+ * @return A list of items
+ */
+@Override
+public List<Items> readAll() {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from items");) {
+		List<Items> items = new ArrayList<>();
+		while (resultSet.next()) {
+			items.add(modelFromResultSet(resultSet));
+		}
+		return items;
+	} catch (SQLException e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return new ArrayList<>();
+}
+
+public Items readLatest() {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+		resultSet.next();
+		return modelFromResultSet(resultSet);
+	} catch (Exception e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return null;
+}
+
+/**
+ * Creates an item in the database
+ * 
+ * @param item - takes in an item object. id will be ignored
+ */
+@Override
+public Items create(Items item) {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();) {
+		statement.executeUpdate("INSERT INTO items(item_name, price) values('" + item.getItemName()
+				+ "','" + item.getPrice() + "')");
+		return readLatest();
+	} catch (Exception e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return null;
+}
+
+public Items readItems(Long id) {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM items where id = " + id);) {
+		resultSet.next();
+		return modelFromResultSet(resultSet);
+	} catch (Exception e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return null;
+}
+
+/**
+ * Updates an item in the database
+ * 
+ * @param item - takes in an item object, the id field will be used to
+ *                 update that item in the database
+ * @return
+ */
+@Override
+public Items update(Items item) {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();) {
+		statement.executeUpdate("update items set item_name ='" + item.getItemName() + "', cost ='"
+				+ item.getPrice() + "' where id =" + item.getId());
+		return readItems(item.getId());
+	} catch (Exception e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return null;
+}
+
+/**
+ * Deletes an item in the database
+ * 
+ * @param id - id of the item
+ */
+@Override
+public int delete(long id) {
+	try (Connection connection = DBUtils.getInstance().getConnection();
+			Statement statement = connection.createStatement();) {
+		return statement.executeUpdate("delete from items where id = " + id);
+	} catch (Exception e) {
+		LOGGER.debug(e);
+		LOGGER.error(e.getMessage());
+	}
+	return 0;
+}
+
+
 }
